46f5f45fac26cfe58a4f6cfc9ae60497af5798a6
Arvind Kasale <arvindkasale@gmail.com>
2022-06-21 19:36:25 +0530

    Complete solution compare + sum sorted array duplicates

--- array/java/arvind/RemoveDuplicatesSortedArray.java
+++ array/java/arvind/RemoveDuplicatesSortedArray.java
@@ -26,6 +26,46 @@ public class RemoveDuplicatesSortedArray {
 	// TC: O(n) SC: O(1)
 	public void removeDuplicates(int[] arr) {
 
+		int j = 0;
+
+		for (int i=1; i< arr.length - 1; i++) {
+			if (arr[i] != arr[i+1]) {
+				arr[j] = arr[i];
+				j++;
+			}
+		}
+
+		arr[j] = arr[arr.length - 1];
+		j++;
+		while (j < arr.length) {
+			arr[j] = 0;
+			j++;
+		}
+	}
+
+	public void removeDuplicates2(int[] arr) {
+
+		int j = 0;
+
+		for (int i=0; i< arr.length; i++) {
+			if (arr[i] > arr[j]) {
+				j++;
+				swap(arr, i, j);
+			}
+		}
+
+		j++;
+
+		while (j < arr.length) {
+			arr[j] = 0;
+			j++;
+		}
+	}
+
+	private void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 
 	public void display(int[] arr) {
@@ -44,5 +84,9 @@ public class RemoveDuplicatesSortedArray {
 		int[] arr2 = new int[] {1, 1, 1, 1, 3, 3, 3, 4, 5, 5, 5, 8, 8, 9, 10, 10};
 		rdsa.removeDuplicates(arr2);
 		rdsa.display(arr2);
+
+		int[] arr3 = new int[] {1, 1, 1, 1, 3, 3, 3, 4, 5, 5, 5, 8, 8, 9, 10, 10};
+		rdsa.removeDuplicates2(arr3);
+		rdsa.display(arr3);
 	}
 }
