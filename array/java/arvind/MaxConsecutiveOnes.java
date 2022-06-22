2262788c8db80566f99558fee06f81c05f5df1cb
Arvind Kasale <arvindkasale@gmail.com>
2022-06-22 19:47:51 +0530

    Add max consecutive ones

--- array/java/arvind/MaxConsecutiveOnes.java
+++ array/java/arvind/MaxConsecutiveOnes.java
@@ -2,11 +2,28 @@ package arvind;
 
 public class MaxConsecutiveOnes {
 
-    private int findMaxConsecutiveOnes() {
+    private int findMaxConsecutiveOnes(int[] arr) {
+        int maxSoFar = 0;
+        int maxCurrent = 0;
 
+        for (int i=0; i< arr.length; i++) {
+            if (arr[i] == 1) {
+                maxCurrent++;
+            } else {
+                if (maxCurrent > maxSoFar) {
+                    maxSoFar = maxCurrent;
+                }
+                maxCurrent = 0;
+            }
+        }
+
+        return Math.max(maxCurrent, maxSoFar);
     }
 
     public static void main(String[] args) {
         MaxConsecutiveOnes mo = new MaxConsecutiveOnes();
+        int[] arr = new int[] {1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1};
+        int result = mo.findMaxConsecutiveOnes(arr);
+        System.out.println("Maximum number of consecutive 1s is "+ result);
     }
 }
