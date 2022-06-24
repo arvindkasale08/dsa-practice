444c38f7f78b079bdcb25eb1092212063562edb6
Anjani100 <anjani10.kumar@gmail.com>
2022-06-24 09:01:03 +0530

    3 Sum Closest Python code added

---
+++ array/python/3SumClosest.py
@@ -0,0 +1,24 @@
+def threeSumClosest(nums, target):
+    res = 9999
+    nums.sort()
+
+    for i in range(len(nums)):
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            threeSum = nums[i] + nums[l] + nums[r]
+            if abs(target - threeSum) < abs(res):
+                res = target - threeSum
+            if threeSum < target:
+                l += 1
+            else:
+                r -= 1
+            if res == 0:
+                break
+    return target - res
+
+if __name__=='__main__':
+    test_cases = [[[-1,2,1,-4], 1],
+                  [[0,0,0], 1],
+                  [[1,1,-1,-1,3], -1]]
+    for nums, target in test_cases:
+        print(threeSumClosest(nums, target))
