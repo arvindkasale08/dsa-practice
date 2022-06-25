9b00ccf4666b1bbe485ecd4cea90cd4fbd4fa6b3
tanmay451 <tanmaypiyushkaushik451@gmail.com>
2022-06-25 13:31:47 -0700

    added code for buy and sell stock, contain most water, gas station and min platform in java

---
+++ array/java/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+public class ContainerWithMostWater {
+    int maxArea(int height[]){
+        int mArea = 0;
+        int i = 0;
+        int j = height.length-1;
+        while(i != j){
+            mArea = Math.max(mArea,(j-i)* Math.min(height[i],height[j]));
+            if (height[i]>height[j]){
+                j -= 1;
+            }else{
+                i += 1;
+            }
+        }
+        return mArea;
+    }
+
+    public static void main(String[] args){
+        ContainerWithMostWater cw = new ContainerWithMostWater();
+        int height[] = new int[]{1,8,6,2,5,4,8,3,7};
+        System.out.println(cw.maxArea(height));
+
+    }
+}
