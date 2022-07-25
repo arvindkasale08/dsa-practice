151a1b4d8cde21d4c34644de9500bea4a99c99a3
tanmay451 <tanmaypiyushkaushik451@gmail.com>
2022-07-26 00:31:22 +0530

    change return type to int

--- binary_tree/KthLargestElement.py
+++ binary_tree/KthLargestElement.py
@@ -4,23 +4,23 @@ class Node:
         self.left = None
         self.right = None
           
-def kthLargestUtil(root, k, c):
-    if root == None or c[0] >= k: 
+def kthLargestUtil(root, k, c, v):
+    
+    if root == None or c[0]>=k: 
         return
-    kthLargestUtil(root.right, k, c)
-  
+
+    kthLargestUtil(root.left,k, c, v)
     c[0] += 1
-  
-    if c[0] == k:
-        print(root.key) 
+    if (c[0] == k):
+        v[0] = root.key
         return
-  
-    kthLargestUtil(root.left, k, c)
+    kthLargestUtil(root.right,k,c, v)
   
 def kthLargest(root, k):
     c = [0]
-    kthLargestUtil(root, k, c)
-  
+    v = [0]
+    kthLargestUtil(root, k, c, v)
+    return v[0]  
 def insert(node, key): 
       
     if node == None:
@@ -43,4 +43,4 @@ if __name__ == '__main__':
     insert(root, 60)
     insert(root, 80)
   
-    kthLargest(root, 3)
+    print(kthLargest(root, 3))
