5045da234a405c488aa0445f368e58e02728d8e6
Arvind Kasale <arvindkasale@gmail.com>
2022-09-05 16:04:58 +0530

    Group anagrams

--- hash_heap/java/src/arvind/GroupAnagrams.java
+++ hash_heap/java/src/arvind/GroupAnagrams.java
@@ -18,6 +18,32 @@ public class GroupAnagrams {
         return result;
     }
 
+    public Map<String, List<String>> solve(String[] arr) {
+        Map<String, List<String>> result = new HashMap<>();
+        for (String s : arr) {
+            int[] asciiIndex = new int[256];
+            for (char c : s.toCharArray()) {
+                asciiIndex[c] +=1;
+            }
+            String sorted = "";
+            for (int i=0; i<asciiIndex.length; i++) {
+                if (asciiIndex[i] > 0) {
+                    char x = (char) i;
+                    int itr =0;
+                    while (itr < asciiIndex[i]) {
+                        sorted += x;
+                        itr++;
+                    }
+                }
+            }
+            if (!result.containsKey(sorted)) {
+                result.put(sorted, new ArrayList<>());
+            }
+            result.get(sorted).add(s);
+        }
+        return result;
+    }
+
     public static void display(Map<String, List<String>> result) {
         for(Map.Entry<String, List<String>> entry : result.entrySet()) {
             System.out.print(entry.getKey() + " - ");
@@ -32,7 +58,7 @@ public class GroupAnagrams {
     public static void main(String[] args) {
         String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
         GroupAnagrams solution = new GroupAnagrams();
-        Map<String, List<String>> result = solution.solveBF(input);
+        Map<String, List<String>> result = solution.solve(input);
         display(result);
     }
 }
