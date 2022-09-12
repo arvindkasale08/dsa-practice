582619f7d88ae47a7515297f77eb5975858d332c
Arvind Kasale <arvindkasale@gmail.com>
2022-09-12 20:07:20 +0530

    Powerset revision

--- Backtracking/java/arvind/PowerSet.java
+++ Backtracking/java/arvind/PowerSet.java
@@ -1,11 +1,13 @@
 package arvind;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PowerSet {
 
     public List<List<Integer>> findPowerSet(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         findPowerSet(nums, list, result, 0);
@@ -15,6 +17,8 @@ public class PowerSet {
     public void findPowerSet(int[] nums, List<Integer> list, List<List<Integer>> result, int j) {
         result.add(new ArrayList<>(list));
         for (int i= j; i<nums.length; i++) {
+            if (i != nums.length-1 && nums[i] == nums[i+1])
+                continue;
             list.add(nums[i]);
             findPowerSet(nums, list, result, i+1);
             list.remove(list.size()-1);
@@ -31,7 +35,7 @@ public class PowerSet {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
+        int[] nums = {1, 2, 3, 2, 2, 3, 1};
         List<List<Integer>> result = new PowerSet().findPowerSet(nums);
         display(result);
     }
