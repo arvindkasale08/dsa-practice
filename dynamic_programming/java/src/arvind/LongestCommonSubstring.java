8f25938a137ccfef15db0287ed63ca0e310544ae
Arvind Kasale <arvind.kasale@appdirect.com>
2022-10-06 21:58:32 +0530

    Fix bug

--- dynamic_programming/java/src/arvind/LongestCommonSubstring.java
+++ dynamic_programming/java/src/arvind/LongestCommonSubstring.java
@@ -7,12 +7,12 @@ public class LongestCommonSubstring {
 		char[] ch2 = s2.toCharArray();
 		int m = ch1.length;
 		int n = ch2.length;
-		int[][] dp = new int[n][m];
+		int[][] dp = new int[n + 1][m + 1];
 		int count = 0;
 
 		for (int i=1; i<dp.length; i++) {
 			for (int j=1; j<dp[0].length; j++) {
-				if (ch1[j] == ch2[i]) {
+				if (ch1[j-1] == ch2[i-1]) {
 					dp[i][j] = 1 + dp[i-1][j-1];
 					count = Math.max(count, dp[i][j]);
 				} else {
