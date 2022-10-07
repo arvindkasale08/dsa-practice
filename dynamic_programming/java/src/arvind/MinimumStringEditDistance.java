9aba56fe7625cb9fe4415624918734e225b248f0
Arvind Kasale <arvindkasale@gmail.com>
2022-10-07 16:45:13 +0530

    Minimum edit distance

---
+++ dynamic_programming/java/src/arvind/MinimumStringEditDistance.java
@@ -0,0 +1,38 @@
+package arvind;
+
+public class MinimumStringEditDistance {
+
+    public int findMinimumDistance(String s1, String s2) {
+        char[] ch1 = s1.toCharArray();
+        char[] ch2 = s2.toCharArray();
+        int[][] dp = new int[ch2.length + 1][ch1.length + 1];
+
+        for (int i=0; i<dp.length; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i=0; i<dp[0].length; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i=1; i<dp.length; i++) {
+            for (int j=1; j<dp[0].length; j++) {
+                if (ch1[j-1] == ch2[i-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
+                }
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+
+    public static void main(String[] args) {
+        MinimumStringEditDistance solution = new MinimumStringEditDistance();
+        String s1 = "SUNDAY";
+        String s2 = "SATURDAY";
+
+        int dist = solution.findMinimumDistance(s1, s2);
+        System.out.println(dist);
+    }
+}
