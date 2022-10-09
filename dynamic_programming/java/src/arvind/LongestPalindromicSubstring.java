6f549f6642ef9316cd977949eefa6933b4c03ddb
Arvind Kasale <arvindkasale@gmail.com>
2022-10-09 18:55:38 +0530

    Longest Palindromic substring

---
+++ dynamic_programming/java/src/arvind/LongestPalindromicSubstring.java
@@ -0,0 +1,31 @@
+package arvind;
+
+public class LongestPalindromicSubstring {
+
+    // returns an int how to convert to return a string
+    public int findLPSubstring(String s1) {
+        char[] ch1 = s1.toCharArray();
+        char[] ch2 = new StringBuilder(s1).reverse().toString().toCharArray();
+        int[][] dp = new int[ch1.length + 1][ch2.length + 1];
+        int k, l = 0;
+        int maxLength = 0;
+        for (int i=1; i< dp.length; i++) {
+            for (int j=1; j<dp[0].length; j++) {
+                if (ch1[j-1] == ch2[i-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    if (maxLength < dp[i][j]) {
+                        maxLength = dp[i][j];
+                    }
+                }
+            }
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        LongestPalindromicSubstring solution = new LongestPalindromicSubstring();
+        String s1 = "babad";
+        int result = solution.findLPSubstring(s1);
+        System.out.println(result);
+    }
+}
