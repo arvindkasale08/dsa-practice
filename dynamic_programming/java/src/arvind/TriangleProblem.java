c5a8347adc63cdcf38b7fdbdcfc4525426140332
Arvind Kasale <arvindkasale@gmail.com>
2022-10-17 13:09:59 +0530

    Triangular problem

--- dynamic_programming/java/src/arvind/TriangleProblem.java
+++ dynamic_programming/java/src/arvind/TriangleProblem.java
@@ -32,6 +32,28 @@ public class TriangleProblem {
         return dp[i][j] = Math.min(down, diagright);
     }
 
+    public int findMinPathSumTab(int[][] triangle) {
+        int m = triangle.length;
+        int n= m;
+        int[][] dp = new int[m][n];
+
+        // populate the base case ie the last row
+        for (int i=0; i<n; i++) {
+            dp[m-1][i] = triangle[m-1][i];
+        }
+
+        for (int i=m-2; i>=0; i--) {
+            for (int j=0; j<=i; j++) {
+                int down = dp[i+1][j] + triangle[i][j];
+                int diagright = dp[i+1][j+1] + triangle[i][j];
+
+                dp[i][j] = Math.min(down, diagright);
+            }
+        }
+
+        return dp[0][0];
+    }
+
     public static void main(String[] args) {
         TriangleProblem solution = new TriangleProblem();
         int[][] triangle = new int[][] {
@@ -41,6 +63,8 @@ public class TriangleProblem {
                 {4, 1, 8, 3}
         };
         int minPathSum = solution.findMinPathSum(triangle);
+        int minPathSum2 = solution.findMinPathSumTab(triangle);
         System.out.println(minPathSum);
+        System.out.println(minPathSum2);
     }
 }
