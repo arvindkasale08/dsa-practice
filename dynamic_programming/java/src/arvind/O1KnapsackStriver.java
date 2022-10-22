88bd2cd60d93c739b05f3d5de8f424bac45d0bea
Arvind Kasale <arvind.kasale@appdirect.com>
2022-10-22 18:13:57 +0530

    Faling tab knapsac

--- dynamic_programming/java/src/arvind/O1KnapsackStriver.java
+++ dynamic_programming/java/src/arvind/O1KnapsackStriver.java
@@ -38,12 +38,49 @@ public class O1KnapsackStriver {
 		return dp[index][c] = Math.max(take, donttake);
 	}
 
+	private int maxValueTab(int[] w, int[] v, int c) {
+		int n = w.length;
+		int[][] dp = new int[n][c+1];
+
+		// Add base cases;
+		for (int j=0; j<dp[0].length; j++) {
+			if (j <= c) {
+				dp[0][j] = v[0];
+			} else {
+				dp[0][j] = v[0];
+			}
+		}
+
+		/*for (int i=0; i<dp.length; i++) {
+			dp[i][0] = 0;
+		}*/
+
+		for (int i=1; i<dp.length; i++) {
+			for (int j=0; j<=dp[0].length; j++) {
+				int donttake = 0 + dp[i -1][j];
+
+				int take = Integer.MIN_VALUE;
+
+				if (w[i] <= c) {
+					take = v[i] + dp[i-1][j - w[i]];
+				}
+
+				dp[i][j] = Math.max(take, donttake);
+			}
+		}
+
+
+		return dp[dp.length -1][dp[0].length - 1];
+	}
+
 	public static void main(String[] args) {
 		O1KnapsackStriver solution = new O1KnapsackStriver();
 		int[] w = new int[] {1, 2, 4, 5};
 		int[] v = new int[] {5, 4, 8, 6};
 		int c = 5;
 		int stolenValue = solution.maxValue(w, v, c);
+		int stolenValue2 = solution.maxValueTab(w, v, c);
 		System.out.println(stolenValue);
+		System.out.println(stolenValue2);
 	}
 }
