938f7f9a5419b6f73959b2706056c57a34094cda
Arvind Kasale <arvind.kasale@appdirect.com>
2022-10-23 18:15:36 +0530

    Add tabulation

--- dynamic_programming/java/src/arvind/UnboundedKnapsackAll.java
+++ dynamic_programming/java/src/arvind/UnboundedKnapsackAll.java
@@ -31,12 +31,36 @@ public class UnboundedKnapsackAll {
 		return dp[index][cap] = Math.max(take, nottake);
 	}
 
+	public int optimiseTab(int[] wt, int[] val, int cap) {
+		int n = wt.length;
+		int[][] dp = new int[n][cap+1];
+		// add basecases
+		for (int j=0; j<dp[0].length; j++) {
+			dp[0][j] = (j / wt[0]) * val[0];
+		}
+
+		for (int i=1; i< dp.length; i++) {
+			for (int j=0; j<dp[0].length; j++) {
+				int nottake = 0 + dp[i-1][j];
+				int take = Integer.MIN_VALUE;
+				if (wt[i] <= j) {
+					take = val[i] + dp[i][j - wt[i]];
+				}
+				dp[i][j] = Math.max(take, nottake);
+			}
+		}
+
+		return dp[dp.length - 1][dp[0].length - 1];
+	}
+
 	public static void main(String[] args) {
 		UnboundedKnapsackAll solution = new UnboundedKnapsackAll();
 		int[] wt = {2, 4, 6};
 		int[] val = {5, 11, 13};
 		int cap = 10;
 		int value = solution.optimise(wt, val, cap);
+		int value2 = solution.optimiseTab(wt, val, cap);
 		System.out.println(value);
+		System.out.println(value2);
 	}
 }
