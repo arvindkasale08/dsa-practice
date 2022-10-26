03e616e845418701379359daef968ac32aa76547
Arvind Kasale <arvind.kasale@appdirect.com>
2022-10-26 13:11:32 +0530

    tabulation

--- dynamic_programming/java/src/arvind/DistinctSubsequences.java
+++ dynamic_programming/java/src/arvind/DistinctSubsequences.java
@@ -30,11 +30,39 @@ public class DistinctSubsequences {
 		}
 	}
 
+	public int findCountofSubSequencesTab(String s1, String s2) {
+		int n = s1.length();
+		int m = s2.length();
+		int[][] dp = new int[n+1][m+1];
+
+		for(int i=0;i<n+1;i++){
+			dp[i][0]=1;
+		}
+		for(int i=1;i<m+1;i++){
+			dp[0][i]=0;
+		}
+
+		for(int i=1;i<n+1;i++){
+			for(int j=1;j<m+1;j++){
+
+				if(s1.charAt(i-1)==s2.charAt(j-1))
+					dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]);
+				else
+					dp[i][j] = dp[i-1][j];
+			}
+		}
+
+
+		return dp[n][m];
+	}
+
 	public static void main(String[] args) {
 		DistinctSubsequences solution = new DistinctSubsequences();
 		String s1 = "babgbag";
 		String s2 = "bag";
 		int result = solution.findCountofSubSequences(s1, s2);
+		int result2 = solution.findCountofSubSequencesTab(s1, s2);
 		System.out.println(result);
+		System.out.println(result2);
 	}
 }
