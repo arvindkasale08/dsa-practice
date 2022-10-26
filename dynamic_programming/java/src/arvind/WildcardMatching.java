19b883b2a3b23d30d2820896fb66bd07e326c2c8
Arvind Kasale <arvind.kasale@appdirect.com>
2022-10-26 19:47:57 +0530

    wildcard matching tabulation

--- dynamic_programming/java/src/arvind/WildcardMatching.java
+++ dynamic_programming/java/src/arvind/WildcardMatching.java
@@ -40,11 +40,44 @@ public class WildcardMatching {
 		return true;
 	}
 
+	public boolean isMatchingTab(String pattern, String str) {
+		int m = pattern.length();
+		int n = str.length();
+		boolean[][] dp = new boolean[m+1][n+1];
+
+		// base cases;
+		dp[0][0] = true;
+
+		for (int j=1; j<dp[0].length; j++) {
+			dp[0][j] = false;
+ 		}
+		for (int i=1; i<dp.length; i++) {
+			dp[i][0] = isAllStars(pattern, i-1);
+		}
+
+		for (int i=1; i<dp.length; i++) {
+			for (int j=1; j<dp[0].length; j++) {
+				if (pattern.charAt(i-1) == str.charAt(j-1) || pattern.charAt(i-1) == '?') {
+					dp[i][j] = dp[i-1][j-1];
+				} else {
+					if (pattern.charAt(i-1) == '*') {
+						dp[i][j] = dp[i-1][j] || dp[i][j-1];
+					} else {
+						dp[i][j] = false;
+					}
+				}
+			}
+		}
+		return dp[dp.length - 1][dp[0].length-1];
+	}
+
 	public static void main(String[] args) {
 		WildcardMatching solution = new WildcardMatching();
-		String s1 = "*?ay";
+		String s1 = "?ay";
 		String s2 = "ray";
 		boolean result = solution.isMatching(s1, s2);
+		boolean result2 = solution.isMatchingTab(s1, s2);
 		System.out.println(result);
+		System.out.println(result2);
 	}
 }
