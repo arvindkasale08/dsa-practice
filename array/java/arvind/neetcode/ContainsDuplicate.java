ae956aee2b18a40f27cd194fd245596df551f5d9
Arvind Kasale <arvindkasale@gmail.com>
2023-01-14 11:16:41 +0530

    Contains duplicate

---
+++ array/java/arvind/neetcode/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+package arvind.neetcode;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        ContainsDuplicate solution = new ContainsDuplicate();
+        boolean result = solution.containsDuplicate(nums);
+        System.out.println(result);
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i=0; i<nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
