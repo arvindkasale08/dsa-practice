6e121555d897451be2b4516da348883c391aaf1e
Arvind Kasale <arvindkasale@gmail.com>
2023-02-05 21:53:02 +0530

    Multiply strings incomplete

---
+++ math-geometry/arvind/src/MultiplyStrings.java
@@ -0,0 +1,13 @@
+public class MultiplyStrings {
+
+    public String multiply(String num1, String num2) {
+
+    }
+
+    public static void main(String[] args) {
+        MultiplyStrings solution = new MultiplyStrings();
+        String num1 = "123";
+        String num2 = "456";
+
+    }
+}
