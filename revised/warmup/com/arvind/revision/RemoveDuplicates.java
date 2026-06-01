a3d90824937ae3f2114205c92b37f3cb43775425
Arvind Kasale <arvindkasale@gmail.com>
2026-06-01 19:37:24 +0530

    3 sum

---
+++ revised/warmup/com/arvind/revision/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+package com.arvind.revision;
+
+public class RemoveDuplicates {
+
+    public int moveElements(int[] arr) {
+        // TODO: Write your code here
+        int lastUnique = 0;
+        int uniqueCount = 1;
+        for (int i=0; i< arr.length; i++) {
+            if (arr[i] == arr[lastUnique]) {
+                // ignore
+            } else {
+                uniqueCount +=1;
+                lastUnique +=1;
+                arr[lastUnique] = arr[i];
+            }
+        }
+        return uniqueCount;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {2, 3, 3, 3, 6, 9, 9};
+        int[] arr2= new int[] {2, 2, 2, 11};
+        RemoveDuplicates solution = new RemoveDuplicates();
+        int res = solution.moveElements(arr2);
+        System.out.println(res);
+        for (int i: arr2) {
+            System.out.print(i + ",");
+        }
+    }
+}
---
+++ revised/warmup/com/arvind/revision/SquaringSortedArray.java
@@ -0,0 +1,35 @@
+package com.arvind.revision;
+
+public class SquaringSortedArray {
+
+    public int[] makeSquares(int[] arr) {
+        int n = arr.length;
+        int[] squares = new int[n];
+        // TODO: Write your code here
+        int l = 0;
+        int r = n-1;
+        int x = n-1;
+        while (l <= r) {
+            if (Math.abs(arr[l]) >= Math.abs(arr[r])) {
+                squares[x] = arr[l] * arr[l];
+                l+=1;
+                x-=1;
+            } else {
+                squares[x] = arr[r] * arr[r];
+                r-=1;
+                x-=1;
+            }
+        }
+        return squares;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {-2, -1, 0, 2, 3};
+        int[] arr2 = new int[] {-3, -2, -1};
+        SquaringSortedArray solution = new SquaringSortedArray();
+        int[] res = solution.makeSquares(arr2);
+        for (int r: res) {
+            System.out.print(r + ",");
+        }
+    }
+}
