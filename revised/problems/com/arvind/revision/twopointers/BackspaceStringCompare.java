8b88268bb5d9c6ceb4e540f314a7b5d6546e5c59
Arvind Kasale <arvindkasale@gmail.com>
2026-06-24 20:38:41 +0530

    Clean up backspace compare validation

--- revised/problems/com/arvind/revision/twopointers/BackspaceStringCompare.java
+++ revised/problems/com/arvind/revision/twopointers/BackspaceStringCompare.java
@@ -2,42 +2,6 @@ package com.arvind.revision.twopointers;
 
 public class BackspaceStringCompare {
 
-    public boolean backspaceCompare(String s, String t) {
-        int i = s.length() - 1;
-        int j = t.length() - 1;
-        boolean flag = true;
-        while (flag) {
-            // move to elligible comparison
-            i = ignoreBackspace(i, s, 0);
-            j = ignoreBackspace(j, t, 0);
-            if (i < 0 && j < 0) return true;
-            if (i < 0 || j < 0) return false;
-            if (s.charAt(i) != t.charAt(j)) return false;
-            i--;
-            j--;
-        }
-        if (i >= 0 || j >= 0) return false;
-        return true;
-    }
-
-    private int ignoreBackspace(int i, String s, int offset) {
-        if (i < 0) return i;
-        char c = s.charAt(i);
-        if (c != '#') {
-            if (offset == 0) {
-                return i;
-            } else {
-                return ignoreBackspace(i - 1, s, offset-1);
-            }
-        } else {
-            if (i > 0) {
-                return ignoreBackspace(i-1, s, offset+1);
-            } else {
-                return -1;
-            }
-        }
-    }
-
     public static void main(String[] args) {
         String s1 = "abef###c";
         String s2 = "ad#c";
@@ -47,5 +11,44 @@ public class BackspaceStringCompare {
         String s6 = "bxo#j##tw";
         BackspaceStringCompare solution = new BackspaceStringCompare();
         System.out.println(solution.backspaceCompare(s5, s6));
+    }
+
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        boolean flag = true;
+        while (flag) {
+            // move to elligible comparison
+            i = ignoreBackspace(i, s, 0);
+            j = ignoreBackspace(j, t, 0);
+            if (i < 0 && j < 0)
+                return true;
+            if (i < 0 || j < 0)
+                return false;
+            if (s.charAt(i) != t.charAt(j))
+                return false;
+            i--;
+            j--;
+        }
+        return i < 0 && j < 0;
+    }
+
+    private int ignoreBackspace(int i, String s, int offset) {
+        if (i < 0)
+            return i;
+        char c = s.charAt(i);
+        if (c != '#') {
+            if (offset == 0) {
+                return i;
+            } else {
+                return ignoreBackspace(i - 1, s, offset - 1);
+            }
+        } else {
+            if (i > 0) {
+                return ignoreBackspace(i - 1, s, offset + 1);
+            } else {
+                return -1;
+            }
+        }
     }
 }
